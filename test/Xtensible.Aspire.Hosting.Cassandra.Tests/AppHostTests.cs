21f7d9e93fb983b1ee9bbeaffc75a92a73a1808a
Giovanni Galbo <ggalbo@gmail.com>
2025-03-12 12:26:40 -0400

    add apphost tests

---
+++ test/Xtensible.Aspire.Hosting.Cassandra.Tests/AppHostTests.cs
@@ -0,0 +1,18 @@
+﻿
+using Aspire.Hosting.Testing;
+
+
+namespace Xtensible.Aspire.Hosting.Cassandra.Tests;
+
+public class AppHostTests(CassandraTestAppHostFixture fixture) : IClassFixture<CassandraTestAppHostFixture>
+{
+   
+    [Fact]
+    public async Task get_connection_string_from_running_container()
+    {
+        var connstring = await fixture.GetConnectionString("cassandra");
+        Assert.StartsWith("Contact Points", connstring);
+
+    }
+
+}
---
+++ test/Xtensible.Aspire.Hosting.Cassandra.Tests/CassandraTestAppHostFixture.cs
@@ -0,0 +1,46 @@
+﻿using Aspire.Hosting.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Xtensible.Aspire.Hosting.Cassandra.Tests
+{
+    public class CassandraTestAppHostFixture() : DistributedApplicationFactory(typeof(Projects.Xtensible_Aspire_Hosting_Cassandra_Demo_AppHost)), IAsyncLifetime
+    {
+        public DistributedApplication App { get; private set; } = null!;
+
+        protected override void OnBuilt(DistributedApplication application)
+        {
+            App = application;
+            base.OnBuilt(application);
+        }
+
+        protected override void OnBuilderCreated(DistributedApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.Services.AddLogging(builder =>
+                {
+                    if (Environment.GetEnvironmentVariable("RUNNER_DEBUG") is not null or "1")
+                        builder.SetMinimumLevel(LogLevel.Trace);
+                    else
+                        builder.SetMinimumLevel(LogLevel.Information);
+                })
+                .ConfigureHttpClientDefaults(clientBuilder => clientBuilder.AddStandardResilienceHandler());
+
+            base.OnBuilderCreated(applicationBuilder);
+        }
+
+
+        public Task InitializeAsync() => StartAsync().WaitAsync(TimeSpan.FromMinutes(10));
+
+        public async Task DisposeAsync()
+        {
+            try
+            {
+                await base.DisposeAsync();
+            }
+            catch (Exception)
+            {
+                // Ignore exceptions during disposal
+            }
+        }
+    }
+}
